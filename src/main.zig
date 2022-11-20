e84bb21c58497dc3bde7f87cf3e8a589a04a9117
￴ <>
2022-11-20 14:45:18 +0700

    update

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,10 @@
-const print = @import("std").debug.print;
-const mem = @import("std").mem; 
-pub fn main() void {
-    testLib();
-    print("{d}\n", .{addOne(42)});
+const std = @import("std");
+const testing = std.testing;
+
+export fn add(a: i32, b: i32) i32 {
+    return a + b;
 }
-const hello_world_in_c =
-    \\#include <stdio.h>
-    \\
-    \\int main(int argc, char **argv) {
-    \\    printf("hello world\n");
-    \\    return 0;
-    \\}
-;
-export fn testLib() void {
-    print("{s}", .{hello_world_in_c});
-}
-export fn addOne(number: i32) i32 {
-    return number + 1;
+
+test "basic add functionality" {
+    try testing.expect(add(3, 7) == 10);
 }
